315d92a1cd28ff94320bb8dcc7491e8ac03eec21
Anthony Anjorin <tonyanjorin@googlemail.com>
2018-06-11 13:05:52 +0200

    Added M2T example

---
+++ de.upb.mbse.taxcalculationexample.reporting/src/de/upb/mbse/taxcalculationexample/reporting/GenerateReports.java
@@ -0,0 +1,45 @@
+package de.upb.mbse.taxcalculationexample.reporting;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+
+import reporting.ReportingJob;
+import reporting.impl.ReportingPackageImpl;
+
+public class GenerateReports {
+	public static void main(String[] args) throws IOException {
+		ReportingPackageImpl.init();
+
+		URI jobURI = URI.createFileURI("./instances/ReportingJob.xmi");
+		ResourceSet rs = new ResourceSetImpl();
+		rs.getResourceFactoryRegistry().getExtensionToFactoryMap()//
+				.put(Resource.Factory.Registry.DEFAULT_EXTENSION, new XMIResourceFactoryImpl());
+		Resource jobResource = rs.createResource(jobURI);
+		jobResource.load(null);
+
+		ReportingJob job = (ReportingJob) jobResource.getContents().get(0);
+
+		File output = new File("./instances/reports" + LocalDateTime.now());
+		output.mkdir();
+
+		job.getReports().forEach(report -> {
+			CharSequence content = new ReportGenerator(report).generate();
+			try (PrintWriter out = new PrintWriter(output.getPath() + "/report" + job.getReports().indexOf(report) + ".md")) {
+				out.println(content);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		});
+		
+		System.out.println("Generated " + job.getReports().size() + " reports.");
+	}
+}
---
+++ de.upb.mbse.taxcalculationexample.reporting/src/de/upb/mbse/taxcalculationexample/reporting/ReportGenerator.xtend
@@ -0,0 +1,63 @@
+package de.upb.mbse.taxcalculationexample.reporting
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import reporting.GENDER
+import reporting.Recipient
+import reporting.Report
+
+class ReportGenerator {
+	private val Report r
+
+	new(Report r) {
+		this.r = r
+	}
+
+	def generate() {
+		'''
+			#«r.header»
+			
+			«handleSalutation(r.recipient)»,
+			
+			«generateMainContent»
+			
+			«handleFarewell»
+			
+			---------------------------------------------------
+			`«r.footer»`
+		'''
+	}
+
+	def handleSalutation(Recipient recipient) {
+		'''«IF recipient.gender == GENDER.FEMALE»
+		Sehr geehrte Frau«ELSEIF recipient.gender == GENDER.MALE»
+		Sehr geehrter Herr«ELSE»
+		Guten Tag,«ENDIF»«handleTitle(r.recipient)»«r.recipient.name» «r.recipient.familyName»'''
+	}
+
+	def handleTitle(Recipient recipient) {
+		''' «IF !recipient.title.empty»
+		«recipient.title» «ENDIF»'''
+	}
+
+	def generateMainContent() {
+		'''
+			«FOR event : r.events»
+				- «event.description» am «handleDate(event.date)»
+			«ENDFOR»
+		'''
+	}
+
+	def handleDate(Date date) {
+		val ft = new SimpleDateFormat("d.MM.yyyy zzz");
+		'''«ft.format(date)»'''
+	}
+
+	def handleFarewell() {
+		'''
+			Mit freundlichen Grüßen
+						
+			Ihre BS
+		'''
+	}
+}
